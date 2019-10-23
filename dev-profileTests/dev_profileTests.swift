88cf4beb9fa49f0191d3c19511f79eb416f2dc3e
Odhran Daly <odhran.daly25@mail.dcu.ie>
2019-10-23 23:54:07 +0100

    set up to run test

---
+++ dev-profileTests/dev_profileTests.swift
@@ -0,0 +1,33 @@
+//
+//  dev_profileTests.swift
+//  dev-profileTests
+//
+//  Created by Odhrán Daly on 23/10/2019.
+//  Copyright © 2019 Odhrán Daly. All rights reserved.
+//
+
+import XCTest
+
+class dev_profileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
